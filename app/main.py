70fbe1240848bf5a2cc492759d1b9d5e175e7a4b
suinming <0223314338aa@gmail.com>
2024-10-18 13:00:48 +0800

    codecrafters submit [skip ci]

--- app/main.py
+++ app/main.py
@@ -1,5 +1,7 @@
 import sys
 
+from app.scanner import Scanner
+
 
 def main():
     if len(sys.argv) < 3:
@@ -14,67 +16,9 @@ def main():
         exit(1)
 
     with open(filename) as file:
-        file_contents = file.read()
-
-    # general token
-    t = {
-        "(": "LEFT_PAREN",
-        ")": "RIGHT_PAREN",
-        "{": "LEFT_BRACE",
-        "}": "RIGHT_BRACE",
-        "*": "STAR",
-        ".": "DOT",
-        ",": "COMMA",
-        "+": "PLUS",
-        "-": "MINUS",
-        ";": "SEMICOLON",
-        "/": "SLASH",
-    }
-    # token need advanced check
-    t_advanced = {
-        "=": "EQUAL",
-        "!": "BANG",
-        "<": "LESS",
-        ">": "GREATER",
-    }
-
-    invalid_token_exist = False
-    p = 0
-
-    while len(file_contents):
-        c = file_contents[p]
-        if t.get(c):
-            if p + 1 < len(file_contents) and file_contents[p] == "/" and file_contents[p + 1] == "/":
-                # if comment detect ignore the remaining characters
-                break
-            else:
-                print(f"{t.get(c)} {c} null")
-        elif t_advanced.get(c):
-            if p + 1 < len(file_contents) and file_contents[p + 1] == "=":
-                print(f"{t_advanced.get(c)}_EQUAL {c}= null")
-                p += 1
-            else:
-                print(f"{t_advanced.get(c)} {c} null")
-        else:
-            # invalid token
-            invalid_token_exist = True
-            print(
-                f"[line 1] Error: Unexpected character: {c}",
-                file=sys.stderr,
-            )
-
-        # increment pointer
-        if p + 1 < len(file_contents):
-            p += 1
-        else:
-            break
-
-    print("EOF  null")
-
-    if invalid_token_exist:
-        exit(65)
-    else:
-        exit(0)
+        file_content = file.read()
+        scanner = Scanner(file_content)
+        scanner.scan_tokens()
 
 
 if __name__ == "__main__":
---
+++ app/scanner.py
@@ -0,0 +1,94 @@
+import sys
+
+
+class Scanner:
+    def __init__(self, file: str):
+        self.file = file
+        self.start = 0
+        self.cur = 0
+        self.line = 1
+        self.invalid_token_exist = False
+        self.token_tab = {
+            "(": "LEFT_PAREN",
+            ")": "RIGHT_PAREN",
+            "{": "LEFT_BRACE",
+            "}": "RIGHT_BRACE",
+            "*": "STAR",
+            ".": "DOT",
+            ",": "COMMA",
+            "+": "PLUS",
+            "-": "MINUS",
+            ";": "SEMICOLON",
+        }
+        self.token_match_tab = {
+            "=": "EQUAL",
+            "!": "BANG",
+            "<": "LESS",
+            ">": "GREATER",
+        }
+
+    def advance(self) -> str:
+        c = self.file[self.cur]
+        self.cur += 1
+        return c
+
+    def peek(self):
+        """like the advance, but do not consume the character"""
+        if self.is_at_end():
+            return "\0"
+        return self.file[self.cur]
+
+    def is_at_end(self) -> bool:
+        return self.cur >= len(self.file)
+
+    def add_token(self, token: str, token_name: str):
+        print(f"{token_name} {token} null")
+
+    def match(self, expected_token) -> bool:
+        if self.is_at_end():
+            return False
+        if self.file[self.cur] != expected_token:
+            return False
+        self.cur += 1
+        return True
+
+    def scan_token(self):
+        c: str = self.advance()
+        if c in self.token_tab:
+            self.add_token(token=c, token_name=self.token_tab.get(c, ""))
+        elif c in self.token_match_tab:
+            token: str = c
+            token_name: str = self.token_match_tab.get(c, "")
+            if self.match("="):
+                self.add_token(token=f"{token}=", token_name=f"{token_name}_EQUAL")
+            else:
+                self.add_token(token=c, token_name=token_name)
+        elif c == "/":
+            if self.match("/"):
+                # detect the comment syntax(which is "//"), and comment go until the end of the line
+                while self.peek() != "\n" and not self.is_at_end():
+                    self.advance()
+            else:
+                self.add_token(token=c, token_name="SLASH")
+        elif c in [" ", "\t", "\r"]:
+            pass
+        elif c == "\n":
+            self.line += 1
+        else:
+            self.invalid_token_exist = True
+            print(
+                f"[line 1] Error: Unexpected character: {c}",
+                file=sys.stderr,
+            )
+
+    def scan_tokens(self):
+        while not self.is_at_end():
+            self.start = self.cur
+            self.scan_token()
+
+        print("EOF  null")
+
+        if self.invalid_token_exist:
+            exit(65)
+        else:
+            exit(0)
